16bdb25452e7e0547b64cb49a87bac0a7785cc07
Ahmed Hamoudi <hamoudi0427@gmail.com>
2021-08-28 04:11:38 -0400

    Added pruning table for the final stage.

--- pruning_table.cpp
+++ pruning_table.cpp
@@ -132,3 +132,42 @@ std::map<std::string, int> generateG3Table(int depth, std::vector<std::string> g
 
 	return states;
 }
+
+//generates pruning table from the fully solved state
+std::map<std::string, int> generateSolvedTable(int depth)
+{
+	//only half turns are permissible
+	std::vector<ROT> moves = {ROT::U2, ROT::D2, ROT::F2, ROT::B2, ROT::L2, ROT::R2};
+
+	//map will have the cube state as the key and the depth from the solved state as the value
+	std::map<std::string, int> states;
+
+	//we start from the solved cube and generate moves from there
+	std::string target = "WWWWWWWWWRRRRRRRRRGGGGGGGGGYYYYYYYYYOOOOOOOOOBBBBBBBBB";
+	std::vector<std::string> previous_frontier = {target};
+
+	//generate states
+	for (int i = 1; i <= depth; i++)
+	{
+		std::vector<std::string> frontier;
+		for (auto previous_state : previous_frontier)
+		{
+			for (auto move : moves)
+			{
+				//add to map if not previously discovered
+				std::string new_cube = rotateCube(previous_state, { move });
+				if (states.find(new_cube) == states.end())
+				{
+					states[new_cube] = i;
+
+					//add to frontier
+					frontier.push_back(new_cube);
+				}
+			}
+		}
+
+		previous_frontier = frontier;
+	}
+
+	return states;
+}
--- pruning_table.h
+++ pruning_table.h
@@ -9,5 +9,6 @@
 std::map<std::string, int> generateG1Table(int depth);
 std::map<std::string, int> generateG2Table(int depth);
 std::map<std::string, int> generateG3Table(int depth, std::vector<std::string> g2_corner_orbits);
+std::map<std::string, int> generateSolvedTable(int depth);
 
 #endif
