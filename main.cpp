b837f269f6c4efb4375ccd59766ec01bafd4dc19
Ahmed Hamoudi <hamoudi0427@gmail.com>
2021-09-07 00:25:42 -0400

    Clean up code.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,4 @@
 #include "cube_structure.h"
-#include <string>
-#include <iostream>
 #include "opencv2/opencv.hpp"
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
@@ -23,11 +21,3 @@ int main(void)
 	//apply moves to cube
 	rubiks_cube.applyMoves(webcam);
 }
-
-
-/*
-	to-do:
-	-arrow maker and state checker using generated moves
-	-make interface nicer, have intro when starting, show upcoming moves, make it turn green when face is found, etc
-	--publish extensive on github, refactor and clean up, put in folders, email zhengzou
-*/
